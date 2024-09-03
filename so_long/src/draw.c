9749afc464d56528c7db050f79560d9b458d98fd
Tomas Konecny <tkonecny@c1r4s7.42prague.com>
2024-09-03 14:40:19 +0200

    cleaned up some stuff in files and changed the wall texture

--- so_long/src/draw.c
+++ so_long/src/draw.c
@@ -6,7 +6,7 @@
 /*   By: tkonecny <tkonecny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 12:30:30 by tkonecny          #+#    #+#             */
-/*   Updated: 2024/09/03 14:17:16 by tkonecny         ###   ########.fr       */
+/*   Updated: 2024/09/03 14:33:09 by tkonecny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	load_images(t_con *prg)
 	xpm_t	*player_xpm;
 	xpm_t	*door_xpm;
 	xpm_t	*collectible_xpm;
+	xpm_t	*floor_xpm;
 
 	wall_xpm = mlx_load_xpm42("/nfs/homes/tkonecny/core/so_long/textures/wall.xpm42");
 	prg->images.wall = mlx_texture_to_image(prg->mlxptr, &wall_xpm->texture);
@@ -33,6 +34,9 @@ void	load_images(t_con *prg)
 	prg->images.collectible = mlx_texture_to_image(prg->mlxptr,
 			&collectible_xpm->texture);
 	mlx_delete_xpm42(collectible_xpm);
+	floor_xpm = mlx_load_xpm42("/nfs/homes/tkonecny/core/so_long/textures/floor.xpm42");
+	prg->images.floor = mlx_texture_to_image(prg->mlxptr, &floor_xpm->texture);
+	mlx_delete_xpm42(floor_xpm);
 }
 
 void	draw_map(t_con *prg)
@@ -43,12 +47,17 @@ void	draw_map(t_con *prg)
 		prg->map.y_load = 0;
 		while (prg->map.layout[prg->map.x_load][prg->map.y_load])
 		{
+			if (prg->map.layout[prg->map.x_load][prg->map.y_load] != '1'
+				|| prg->map.layout[prg->map.x_load][prg->map.y_load] != 'E'
+				|| prg->map.layout[prg->map.x_load][prg->map.y_load] != 'C')
+				mlx_image_to_window(prg->mlxptr, prg->images.floor,
+					prg->map.y_load * TILE_SIZE, prg->map.x_load * TILE_SIZE);
 			if (prg->map.layout[prg->map.x_load][prg->map.y_load] == '1')
 				mlx_image_to_window(prg->mlxptr, prg->images.wall,
 					prg->map.y_load * TILE_SIZE, prg->map.x_load * TILE_SIZE);
 			// if (prg->map.layout[prg->map.x_load][prg->map.y_load] == 'P')
 			// 	mlx_image_to_window(prg->mlxptr, prg->images.player,
-			// 		prg->map.y_load * TILE_SIZE, prg->map.x_load * TILE_SIZE);
+			// prg->map.y_load * TILE_SIZE, prg->map.x_load * TILE_SIZE);
 			if (prg->map.layout[prg->map.x_load][prg->map.y_load] == 'E')
 				mlx_image_to_window(prg->mlxptr, prg->images.exit,
 					prg->map.y_load * TILE_SIZE, prg->map.x_load * TILE_SIZE);
